9e65000df693fccf048f84eaa9e1bd41ab0568b5
Anshul Tamrakar <anshultamrakar487@gmail.com>
2022-11-23 16:15:42 +0530

    feat : added new pages

---
+++ hypotenues.js
@@ -0,0 +1,9 @@
+const btnCheck = document.querySelector("#btn-check")
+
+
+function calculateHypotenues(){
+    
+}
+
+
+btnCheck.addEventListener("click" , calculateHypotenues)
--- quiz.js
+++ quiz.js
@@ -1,10 +1,23 @@
-const btnSubmit = document.querySelector("#submit-btn")
-console.log(btnSubmit)
+const quizForm  = document.querySelector(".quiz-form")
+const btnSubmit = document.querySelector("#submit-btn");
+const outputElement  = document.querySelector('#output')
+console.log(outputElement)
 
+const correctAnswers = ["90", "right-angle-triangle"]
 
-
-function submitHandler(e){
-  e.preventDefault()
+function calculateScore(){
+    let score = 0 ;
+    let index = 0 ;
+    const formResult = new FormData(quizForm)
+    for(let value of formResult.values()){
+      if(value === correctAnswers[index]){
+            score = score + 1 
+      }
+         index = index + 1
+    }
+    outputElement.innerHTML = "Your current score is " + score;
 }
 
-btnSubmit.addEventListener("click", submitHandler)
+
+
+btnSubmit.addEventListener("click", calculateScore )
